765168ed5f349ac6a81f364720c19dec33b23457
Ro-Nel <rodrigo.saravia@ucb.edu.bo>
2021-05-25 17:16:49 -0400

    Creación de Spring Boot

---
+++ src/main/java/ucb/ingsoft/demopet/DemoPetApplication.java
@@ -0,0 +1,13 @@
+package ucb.ingsoft.demopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucb/ingsoft/demopet/DemoPetApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.ingsoft.demopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
